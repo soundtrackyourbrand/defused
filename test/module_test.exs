22d744f0948841f9c3d4fb576057e4ed2b146d66
Fredrik Enestad <fredrik.enestad@soundtrackyourbrand.com>
2017-08-23 11:11:23 +0200

    blown error overridable

--- test/module_test.exs
+++ test/module_test.exs
@@ -2,7 +2,6 @@ defmodule Defused.ModuleTest do
   use ExUnit.Case
   @fuse :defused_module_fuse
 
-
   defmodule Test do
     def foo(_a, _b) do
       {:ok, :hello}
@@ -28,8 +27,21 @@ defmodule Defused.ModuleTest do
     use Defused.Module, target: Test, fuse: @fuse, except: [foo: 2]
   end
 
+  defmodule DefusedTestError do
+    @fuse :defused_module_fuse
+    use Defused.Module, target: Test, fuse: @fuse
+
+    def blown_error(fuse, call) do
+      {:error, fuse, call}
+    end
+  end
+
   setup_all do
-    :fuse.install(@fuse, {{:standard, 1, 1000}, {:reset, 10_000}})
+    :fuse.install(@fuse, {{:standard, 10, 1000}, {:reset, 10_000}})
+  end
+
+  setup do
+    :ok = :fuse.circuit_enable(@fuse)
   end
 
   test "ok functions" do
@@ -44,10 +56,22 @@ defmodule Defused.ModuleTest do
   end
 
   test "only is respected" do
-    assert DefusedTestOnly.__info__(:functions) == [foo: 2]
+    assert funcs(DefusedTestOnly) == [foo: 2]
   end
 
   test "except is respected" do
-    assert DefusedTestExcept.__info__(:functions) == [bar: 0]
+    assert funcs(DefusedTestExcept) == [bar: 0]
+  end
+
+  test "can override blown error" do
+    assert DefusedTestError.bar() == {:error, :broken}
+    :ok = :fuse.circuit_disable(@fuse)
+    assert DefusedTestError.bar() == {:error, @fuse, [bar: 0]}
+  end
+
+  def funcs(target) do
+    target.__info__(:functions) |> Enum.reject(fn {f, a} ->
+      f == :blown_error && a == 2
+    end)
   end
 end
